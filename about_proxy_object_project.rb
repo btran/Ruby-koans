82283c7d76781d819b9775f35f27838d0f4a2f21
btran <bryantran7@gmail.com>
2011-12-19 20:21:38 -0500

    Done!

--- about_proxy_object_project.rb
+++ about_proxy_object_project.rb
@@ -13,12 +13,26 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_accessor :messages
   def initialize(target_object)
     @object = target_object
-    # ADD MORE CODE HERE
+    @messages=[]
   end
 
-  # WRITE CODE HERE
+  def method_missing(method_name, *args, &block)
+      @messages << method_name.to_sym
+    if @object.respond_to?(method_name.to_sym)
+      @object.send(method_name.to_sym, *args,&block)
+    else
+      super(method_name,*args,&block)
+    end
+  end
+  def called?(arg)
+     @messages.include? arg
+  end
+  def number_of_times_called(arg)
+    @messages.count arg
+  end
 end
 
 # The proxy object should pass the following Koan:
@@ -27,50 +41,50 @@ class AboutProxyObjectProject < EdgeCase::Koan
   def test_proxy_method_returns_wrapped_object
     # NOTE: The Television class is defined below
     tv = Proxy.new(Television.new)
-    
+
     assert tv.instance_of?(Proxy)
   end
-  
+
   def test_tv_methods_still_perform_their_function
     tv = Proxy.new(Television.new)
-    
+
     tv.channel = 10
     tv.power
-    
+
     assert_equal 10, tv.channel
     assert tv.on?
   end
 
   def test_proxy_records_messages_sent_to_tv
     tv = Proxy.new(Television.new)
-    
+
     tv.power
     tv.channel = 10
-    
+
     assert_equal [:power, :channel=], tv.messages
   end
-  
+
   def test_proxy_handles_invalid_messages
     tv = Proxy.new(Television.new)
-    
+
     assert_raise(NoMethodError) do
       tv.no_such_method
     end
   end
-  
+
   def test_proxy_reports_methods_have_been_called
     tv = Proxy.new(Television.new)
-    
+
     tv.power
     tv.power
-    
+
     assert tv.called?(:power)
     assert ! tv.called?(:channel)
   end
-  
+
   def test_proxy_counts_method_calls
     tv = Proxy.new(Television.new)
-    
+
     tv.power
     tv.channel = 48
     tv.power
@@ -99,7 +113,7 @@ end
 # Example class using in the proxy testing above.
 class Television
   attr_accessor :channel
-  
+
   def power
     if @power == :on
       @power = :off
@@ -107,7 +121,7 @@ class Television
       @power = :on
     end
   end
-  
+
   def on?
     @power == :on
   end
@@ -117,31 +131,31 @@ end
 class TelevisionTest < EdgeCase::Koan
   def test_it_turns_on
     tv = Television.new
-    
+
     tv.power
     assert tv.on?
   end
-  
+
   def test_it_also_turns_off
     tv = Television.new
-    
+
     tv.power
     tv.power
-    
+
     assert ! tv.on?
   end
-  
+
   def test_edge_case_on_off
     tv = Television.new
-    
+
     tv.power
     tv.power
     tv.power
-        
+
     assert tv.on?
-    
+
     tv.power
-    
+
     assert ! tv.on?
   end
 
